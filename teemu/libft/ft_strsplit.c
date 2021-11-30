4084def0202288a2f6fa29064a32d92f86ea8eae
Jere Turunen <jturunen@student.hive.fi>
2021-11-30 12:09:54 +0200

    additional crap

---
+++ teemu/libft/ft_strsplit.c
@@ -0,0 +1,164 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jturunen <jturunen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/19 14:22:18 by jturunen          #+#    #+#             */
+/*   Updated: 2021/11/29 12:31:11 by jturunen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static unsigned int	c_strlen(char const *s, char c)
+{
+	unsigned int	count;
+	unsigned int	i;
+
+	count = 0;
+	i = 0;
+	while (s[i])
+	{
+		while (s[i] == c && s[i])
+			i++;
+		if (!s[i])
+			return (count);
+		count++;
+		while (s[i] != c && s[i])
+			i++;
+	}
+	return (count);
+}
+
+/*static unsigned int	c_strlen(char const *s, char c)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+	return (i);
+}*/
+
+char	**ft_strsplit(char const *s, char c)
+{
+	char			**tab;
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	start;
+
+	if (!s)
+		return (NULL);
+	tab = (char **)malloc(sizeof(char *) * c_strlen(s, c) + sizeof(char));
+	if (!tab)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		while (s[i] == c && s[i])
+			i++;
+		start = i;
+		if (!s[i])
+			break ;
+		while (s[i] != c && s[i])
+			i++;
+		tab[j++] = ft_strsub(s, start, i - start);
+	}
+	tab[j] = 0;
+	return (tab);
+}
+
+/*static char	*c_strdup(const char *s, int start, int finish)
+{
+	char			*str;
+	unsigned int	i;
+
+	i = 0;
+	str = malloc((sizeof(char) * (finish - start + 1)));
+	if (!str)
+		return (NULL);
+	while (start < finish)
+		str[i++] = s[start++];
+	str[i] = '\0';
+	return (str);
+}
+
+static unsigned int	word_count(char const *s, char c)
+{
+	unsigned int	i;
+	unsigned int	word;
+
+	i = 0;
+	word = 0;
+	while (*s)
+	{
+		if ((*s == c) && (word == 1))
+			word = 0;
+		if ((word == 0) && (*s != c))
+		{
+			word = 1;
+			i++;
+		}
+		s++;
+	}
+	return (i);
+}
+
+char	**ft_strsplit(const char *s, char c)
+{
+	int				start;
+	unsigned int	i;
+	unsigned int	j;
+	char			**tab;
+
+	if (!s)
+		return (NULL);
+	if (!(tab = malloc(sizeof(char *) * (word_count(s, c) + 1))))
+		return (NULL);
+	i = 0;
+	j = 0;
+	start = -1;
+	while (i <= ft_strlen(s))
+	{
+		if (s[i] != c && start < 0)
+			start = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && start >= 0)
+		{
+			tab[j++] = c_strdup(s, start, i);
+			start = -1;
+		}
+		i++;
+	}
+	tab[j] = 0;
+	return (tab);
+}*/
+
+/*char	**ft_strsplit(const char *s, char c)
+{
+	char	**tab;
+	int		words;
+	int		i;
+
+	if (!s || !c)
+		return (NULL);
+	i = 0;
+	words = word_count(s, c);
+	tab = (char **)malloc(sizeof(char *) * (word_count(s, c) + 1));
+	if (!tab)
+		return (NULL);
+	while (words--)
+	{
+		while ((*s == c) && (*s != '\0'))
+			s++;
+		tab[i] = ft_strsub(s, 0, c_strlen(s, c));
+		if (!tab)
+			return (NULL);
+		s = s + c_strlen(s, c);
+		i++;
+	}
+	tab[i] = NULL;
+	return (tab);
+}*/
---
+++ teemu/libft/main.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jturunen <jturunen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 14:46:53 by jturunen          #+#    #+#             */
+/*   Updated: 2021/11/29 16:07:30 by jturunen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+#include <ctype.h>
+
+int	main(void)
+{
+//	const char	*data;
+	char		**tab1;
+	int			i;
+//	size_t		size;
+//	t_list		*test;
+
+//	data = "1 3 5 7,9";
+//	size = ft_strlen(data) + 1;
+//	test = ft_lstnew(data, size);
+//	printf("ft_lstnew data is: %s\n", (char *)test->content);
+//	ft_putnbr((int)size);
+//	ft_putchar('\n');
+	i = 0;
+	tab1 = ft_strsplit("123456789\0toka\0kolmas\0neljas\0viides\0kuudes", '\0');
+	while (i < 2)
+	{
+		printf("ft_strsplit string %d : %s\n", i, tab1[i]);
+		i++;
+	}
+}
+/*void	owntest(void)
+{
+	const char	*str1;
+	char		str2[256];
+	const char	*cmp1;
+	const char	*cmp2;
+	char		**tab1;
+	int			i;
+
+	str1 = "jou jou";
+	cmp1 = "right";
+	cmp2 = "wrong";
+	printf("\n### MY UBER tests: ###\n\n");
+	ft_putstr("hullooo ppl whadap?!\n");
+	printf("ft_strcmp %d\n", ft_strcmp("right", "wrong"));
+	printf("ft_strcmp %d\n", ft_strcmp(cmp1, cmp2));
+	printf("string to be copied:	%s\n", str1);
+	printf("string that was copied:	%s\n", ft_strcpy(str2, str1));
+	printf("looking for char u: %s\n", ft_strchr(str1, 'u'));
+	printf("looking for char o: %s\n", ft_strrchr(str1, 'o'));
+	ft_putendl("whaaaat");
+	ft_putnbr(12345);
+	printf("ft_strdup str1 to new: %s\n", ft_strdup(str1));
+	printf("ft_strcat test: %s\n", ft_strcat(str2, cmp2));
+	ft_memmove(NULL, NULL, 1);
+	ft_memcpy(NULL, NULL, 1);
+	ft_putnbr(ft_isalnum('1'));
+	ft_putnbr(ft_isalnum('a'));
+	ft_putnbr(ft_isalnum('A'));
+	ft_putnbr(ft_isalnum('2'));
+	ft_putnbr(ft_isalnum('+'));
+	ft_putnbr(ft_isalnum('s'));
+	ft_putchar('\n');
+	i = 0;
+	tab1 = ft_split("\0aa\0bb", '\0');
+	while (i < 2)
+	{
+		printf("ft_strsplit string %d : %s\n", 1, tab1[i]);
+		i++;
+	}
+}
+
+int	main(void)
+{
+	const char	*str1;
+	char		str2[256];
+	const char	*cmp1;
+	const char	*cmp2;
+
+	str1 = "jou jou";
+	cmp1 = "right";
+	cmp2 = "wrong";
+	owntest();
+	printf("\n### LIB-C tests: ###\n\n");
+	printf("isalpha 'C': %d\n", isalpha('C'));
+	printf("strcmp %d\n", strcmp("right", "wrong"));
+	printf("strcmp %d\n", strcmp(cmp1, cmp2));
+//	memmove(NULL, NULL, 1);
+//	memcpy(NULL, NULL, 1);*/
+	/*printf("memmove nul ja literal ja numero \n");		
+	memmove(NULL, "fuckthisshit", 10);					
+	printf("memcpy nul ja nul ja numero \n");			
+	printf("memcpy nul ja literal ja numero \n");		
+	memcpy(NULL, "notfuckingshithuh", 10);				
+	printf("nul ja literal ja numero meni lapi \n");*/
+/*	printf("strdup str1 to new: %s\n", strdup(str1));
+	printf("strcat test: %s\n", strcat(str2, cmp2));
+	return (0);
+}*/
