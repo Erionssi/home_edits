c422505488260a81b4e95a95cc5e746cd5d39423
JTerion Home <jerejt@gmail.com>
2022-01-25 15:29:45 +0200

    small correction to ft_strchr

--- get_next_line/libft/ft_strchr.c
+++ get_next_line/libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: jturunen <jturunen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 14:55:17 by jturunen          #+#    #+#             */
-/*   Updated: 2022/01/25 15:24:55 by jturunen         ###   ########.fr       */
+/*   Updated: 2022/01/25 15:28:57 by jturunen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ char	*ft_strchr(char *s, int c)
 	int	i;
 
 	i = 0;
-	if (!str)
+	if (!s)
 		return (0);
 	if (c == '\0')
-		return ((char *)&str[ft_strlen(str)]);
-	while (str[i] != '\0')
+		return ((char *)&s[ft_strlen(s)]);
+	while (s[i] != '\0')
 	{
-		if (str[i] == (char) c)
-			return ((char *)&str[i]);
+		if (s[i] == (char) c)
+			return ((char *)&s[i]);
 		i++;
 	}
 	return (0);
