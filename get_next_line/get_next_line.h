f4abb75794b58bb7930c26ae8ad8192870c39527
Jere Turunen <jturunen@student.hive.fi>
2022-04-20 08:58:40 +0300

    added some test files etc small modifications. atual GNL is still the same as in vogsphere

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jturunen <jturunen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 13:44:20 by jturunen          #+#    #+#             */
-/*   Updated: 2022/01/25 17:30:16 by jturunen         ###   ########.fr       */
+/*   Updated: 2022/04/06 10:58:24 by jturunen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-# define BUFF_SIZE 8
+# define BUFF_SIZE 32
 # define MAX_FD 4096
 
 int		get_next_line(const int fd, char **line);
